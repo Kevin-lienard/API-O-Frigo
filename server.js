cd1ebbb7fc65c003198529f02d2e3bffd71041ed
Kevin <k.lienard@yahoo.fr>
2023-04-11 00:42:14 +0200

    API O'Frigo v1.0

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-const dotenv = require("dotenv");
-dotenv.config();
 const express = require('express');
 const { recipe } = require("./app/router");
 const app = express();
 
-app.use(express.static('./asset'));
 app.use(express.json());
 
 app.use(recipe);
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = 3000;
 app.listen(PORT, ()=>{
     console.log(`Server running on port: ${PORT}`);
 });
