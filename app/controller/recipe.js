31ea25ab7b1a057efb8aae2a8435ca98bd1d5338
Kevin <k.lienard@yahoo.fr>
2023-04-10 23:17:52 +0200

    API O'Frigo v1.0

--- app/controller/recipe.js
+++ app/controller/recipe.js
@@ -3,7 +3,7 @@ const { Recipe } = require("../model");
 const recipeController = {
 
     getAllRecipe: async (_, res) =>{
-        const recipe = await Recipe.findAll({ include: [ 'recipeTag', 'recipeIngredient', 'recipeStep', 'recipeQuantity' ], order: [ ['id', 'ASC'], ['recipeTag', 'label', 'ASC']] });
+        const recipe = await Recipe.findAll({ include: [ 'recipeTag', 'recipeIngredient', 'recipeStep', 'recipeQuantity' ], order: [ ['id', 'ASC'], ['recipeTag', 'label', 'ASC'], ['recipeIngredient', 'label', 'ASC'] ] });
     
         res.status(200).json(recipe);
     },
