78f3f1f951e8dbd8687903eecde8db3c36c68720
Kevin <k.lienard@yahoo.fr>
2023-04-10 21:15:19 +0200

    API O'Frigo v1.0

--- test.js
+++ test.js
@@ -3,7 +3,6 @@ const { Recipe } = require("./model");
 async function getRecipeWithTags(id){
     const recipeTag = await Recipe.findByPk(id, { include: { association: 'tagRecipe' }, order: [['tagRecipe', 'label', 'ASC']] });
     const recipe = await Recipe.findByPk(id);
-
     const recipeWithTags = recipe.dataValues;
     recipeWithTags.tags = [];
 
