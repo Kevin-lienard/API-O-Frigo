594688e7080aac4f71f1c903c6bb5d1467f58695
Kevin <k.lienard@yahoo.fr>
2023-04-10 23:53:43 +0200

    API O'Frigo v1.0

--- app/controller/recipe.js
+++ app/controller/recipe.js
@@ -3,14 +3,14 @@ const { Recipe } = require("../model");
 const recipeController = {
 
     getAllRecipe: async (_, res) =>{
-        const recipe = await Recipe.findAll({ include: [ 'recipeTag', 'recipeIngredient', 'recipeStep', 'recipeQuantity' ], order: [ ['id', 'ASC'], ['recipeTag', 'label', 'ASC'], ['recipeIngredient', 'label', 'ASC'] ] });
+        const recipe = await Recipe.findAll({ include: [ 'recipeTag', 'recipeIngredient', 'recipeStep', 'recipeQuantity' ], order: [ ['recipeTag', 'label', 'ASC'], ['recipeIngredient', 'label', 'ASC'], ['recipeStep', 'id', 'ASC'], ['recipeQuantity', 'id', 'ASC'] ] });
     
         res.status(200).json(recipe);
     },
 
     getRecipeWithTags: async (req, res) =>{
         const recipeId = req.params.id;
-        const recipe = await Recipe.findByPk(recipeId, { include: [ 'recipeTag', 'recipeIngredient', 'recipeStep', 'recipeQuantity' ], order: [ ['recipeTag', 'label', 'ASC'], ['recipeIngredient', 'label', 'ASC'] ] });
+        const recipe = await Recipe.findByPk(recipeId, { include: [ 'recipeTag', 'recipeIngredient', 'recipeStep', 'recipeQuantity' ], order: [ ['recipeTag', 'label', 'ASC'], ['recipeIngredient', 'label', 'ASC'], ['recipeStep', 'id', 'ASC'], ['recipeQuantity', 'id', 'ASC'] ] });
 
         res.status(200).json(recipe);
     },
